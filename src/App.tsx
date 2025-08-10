35af0ac707f296d803501e92e0407a00bd07b841
jou-coding <jou.moriyama05@gmail.com>
2025-08-11 04:28:35 +0900

    feat(App): Appページのhtml部分完成

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ function App() {
   return (
     <div>
       <h1>Jou-coding</h1>
+      <h1>about</h1>
       <nav>
         <ol>
           <li>about</li>
@@ -9,6 +10,12 @@ function App() {
           <li>contact</li>
         </ol>
       </nav>
+      <div>webアプリの制作をしています。</div>
+      <div>
+        <img src="" alt="画像" />
+        <p>名前:Jou-coding</p>
+        <p>事業内容:フロントエンドとバックエンド</p>
+      </div>
     </div>
   );
 }
