4c7f242106e775973dcbc43be7235ca5e450b01a
jou-coding <Jou.Moriyama05@gmail.com>
2025-08-26 13:51:50 +0900

    fix:merge後の微修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Contact from "./pages/Contact";
 import About from "./pages/about";
 import Works from "./pages/Works";
