fe7662d1aa2a2fa18fc9bcf50cf77ab5cc30e168
jou-coding <Jou.Moriyama05@gmail.com>
2025-08-26 12:10:21 +0900

    fix:名前の修正

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Hedear() {
   return (
     <div className="flex justify-between p-8">
-      <h1>About</h1>
+      <h1>jou-coding</h1>
       <nav>
         <ul className="flex gap-5 ">
           <li className="hover:underline">about</li>
