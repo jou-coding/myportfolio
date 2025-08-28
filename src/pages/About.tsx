e4af70a5e29d8260c3f911ccdb69b899aa0f2835
jou-coding <Jou.Moriyama05@gmail.com>
2025-08-29 08:41:17 +0900

    style:画像や名前、内容を縦並びにする

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,8 +3,8 @@ function About() {
     <div className="flex  justify-center flex-col items-center gap-4">
       <h1 className="text-3xl font-bold ">About</h1>
       <div>webアプリの制作をしています。</div>
-      <div>
-        <img src="" alt="画像" />
+      <div className="flex flex-col justify-center items-center">
+        <img className=" h-25 w-auto" src="./profile.png" alt="画像" />
         <p>名前:jou-coding</p>
         <p>内容:フロントエンドとバックエンド</p>
       </div>
