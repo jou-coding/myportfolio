47bb1fc1d4fee03b63f1963106051e81357ddb43
jou-coding <Jou.Moriyama05@gmail.com>
2025-09-03 08:52:50 +0900

    style:portfolioに変更

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Hedear() {
   return (
     <div className="flex justify-between p-8">
-      <h1>jou-coding</h1>
+      <h1>Portfolio</h1>
       <nav>
         <ul className="flex gap-5 ">
           <li className="hover:underline">
