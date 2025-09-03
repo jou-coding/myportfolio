5b2a1e9e61e928d5c49ac78f189e545cb188c460
jou-coding <Jou.Moriyama05@gmail.com>
2025-09-03 09:19:46 +0900

    feat:Githubのアカウントを追加

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -6,7 +6,13 @@ function About() {
       <div className="flex flex-col justify-center items-center">
         <img className=" h-25 w-auto" src="./profile.png" alt="画像" />
         <p>名前:jou-coding</p>
-        <p>内容:フロントエンドとバックエンド</p>
+        <p>内容:フロントエンドとバックエンドの開発の勉強しています。</p>
+        <p>
+          Github:
+          <a href="https://github.com/jou-coding" className="hover:underline">
+            jou-coding
+          </a>
+        </p>
       </div>
     </div>
   );
